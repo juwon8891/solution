311a8e8d3022f58ca00d66a64e822dba5fe1f788
황주원 <juwon8891@gmail.com>
2022-09-17 09:12:56 +0900

    codingTest

--- src/JadenCase.java
+++ src/JadenCase.java
@@ -1,19 +1,15 @@
 class JadenCase {
         public String solution(String s) {
             String ans = "";
-
             // 모든 문자열 소문자로 변경
             String[] srr = s.toLowerCase().split("");
-
             boolean isFirst = true;
-
-            for(int i = 0; i < srr.length; i++) {
+            for (String value : srr) {
                 // 첫 글자일 경우 대문자로 변경
-                ans += isFirst ? srr[i].toUpperCase() : srr[i];
-                // 공백이 나오면 첫 글자 플그래 true로 변경
-                isFirst = srr[i].egquals(" ") ? true : false;
+                ans += isFirst ? value.toUpperCase() : value;
+                // 공백이 나오면 첫 글자 플래그 true로 변경
+                isFirst = value.equals(" ");
             }
-
             return ans;
         }
     public static void main(String[] args) {
